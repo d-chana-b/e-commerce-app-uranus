fdafbbb2a1dabea8f2c09be09231578542b48ba1
d-chana-b <delossantos.nino.p@gmail.com>
2022-03-02 21:32:36 +0800

    first commit
    uploading the starter package of the project

---
+++ src/main/java/com/myapp/uranuscapstone/UranuscapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.uranuscapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UranuscapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UranuscapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/uranuscapstone/UranuscapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.uranuscapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UranuscapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
