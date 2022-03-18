fa55d3fcc6726ae50977a6fb5f668b75a81d631a
d-chana-b <delossantos.nino.p@gmail.com>
2022-03-18 22:20:57 +0800

    adding checkout and orderdetails page - nino

---
+++ src/main/java/com/myapp/uranuscapstone/service/OrderDetailService.java
@@ -0,0 +1,20 @@
+package com.myapp.uranuscapstone.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myapp.uranuscapstone.model.OrderDetail;
+import com.myapp.uranuscapstone.model.User;
+import com.myapp.uranuscapstone.repository.OrderDetailRepository;
+
+@Service
+public class OrderDetailService {
+	@Autowired
+	OrderDetailRepository orderDetailRepository;
+
+	public List<OrderDetail> index(User user) {
+		return orderDetailRepository.findByUser(user);
+	}
+}
